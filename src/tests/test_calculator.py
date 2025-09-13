c547aaf14804274c63fbceecf5e6912b7dfd9e77
Nicolas Paquette <nicolas.paquette.2@ens.etsmtl.ca>
2025-09-12 23:59:36 -0400

    Tests pour calculator.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -4,10 +4,21 @@ SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 
-from calculator import Calculator
+from src.calculator import Calculator
+
+my_calculator = Calculator()
 
 def test_app():
-    my_calculator = Calculator()
     assert my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
 
-# TODO: ajoutez les tests
+def test_addition():
+    assert my_calculator.addition(2,3) == 5
+
+def test_subtraction():
+    assert my_calculator.subtraction(2,3) == -1
+
+def test_multiplication():
+    assert my_calculator.multiplication(2,3) == 6
+
+def test_division():
+    assert my_calculator.division(4,2) == 2
